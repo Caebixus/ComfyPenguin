5078b4821e0ce463eedb8a1c82b26fe8f7e809a5
Caebix <ma.davidlangr@gmail.com>
2019-02-26 20:14:17 +0100

    admin

--- clothes/admin.py
+++ clothes/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from .models import Product_Clothes
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'item_title', 'item_category','item_gender', 'item_asin', 'item_url', 'upload_date')
+    list_display = ('item_title', 'item_size', 'item_category','item_color', 'item_asin', 'item_url', 'upload_date')
     list_filter = ('item_category', 'item_gender', 'item_seller')
     search_fields = ('item_title', 'item_asin')
 
