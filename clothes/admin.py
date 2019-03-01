d669412246617f02494953624b806bc7fb80522f
Caebix <ma.davidlangr@gmail.com>
2019-03-01 12:43:51 +0100

    admin update save_as

--- clothes/admin.py
+++ clothes/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from .models import Product_Clothes
 
 class PostAdmin(admin.ModelAdmin):
+    save_as = True
     list_display = ('item_title', 'item_size', 'item_category','item_color', 'item_asin', 'item_url', 'upload_date')
     list_filter = ('item_category', 'item_gender', 'item_seller')
     search_fields = ('item_title', 'item_asin')
