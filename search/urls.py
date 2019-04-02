108052bbe3504f743b7af7aa33f510883a9b797f
Caebix <ma.davidlangr@gmail.com>
2019-04-02 14:48:12 +0200

    update

--- search/urls.py
+++ search/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path('filter-mens-tops', views.filter_mens_tops, name='filter_mens_tops'),
     path('mens-tops', views.search_mens_tops, name='search_mens_tops'),
     path('filter-womens-tops', views.filter_womens_tops, name='filter_womens_tops'),
+    path('search_womens_tops_sweatshirts', views.search_womens_tops_sweatshirts, name='search_womens_tops_sweatshirts'),
+    path('search_womens_tops_hoodies', views.search_womens_tops_hoodies, name='search_womens_tops_hoodies'),
+    path('search_womens_tops_jumpers', views.search_womens_tops_jumpers, name='search_womens_tops_jumpers'),
+    path('search_mens_tops_sweatshirts', views.search_mens_tops_sweatshirts, name='search_mens_tops_sweatshirts'),
+    path('search_mens_tops_hoodies', views.search_mens_tops_hoodies, name='search_mens_tops_hoodies'),
+    path('search_mens_tops_jumpers', views.search_mens_tops_jumpers, name='search_mens_tops_jumpers'),
     path('womens-tops', views.search_womens_tops, name='search_womens_tops'),
 ]
--- search/views.py
+++ search/views.py
@@ -45,7 +45,6 @@ def searchpage(request):
         countries = request.GET['Continent']
         if countries:
             queryset_list = queryset_list.filter(item_continent__iexact=countries)
-    queryset_list = queryset_list.filter(item_gender__iexact='Male')
     paginator = Paginator(queryset_list, 16, orphans=4)
     my_total_searched = queryset_list.count()
     page = request.GET.get('page')
@@ -157,6 +156,195 @@ def filter_mens_tops(request):
 
     return render(request, 'filter-mens-tops.html', context)
 
+def search_mens_tops_sweatshirts(request):
+    listing = Product_Clothes_Tops.objects.all()[:8]
+    my_total = Product_Clothes_Tops.objects.count()
+    # counting Mans clothes
+    my_total_men = Product_Clothes_Tops.objects.filter(item_gender__iexact="Male")
+    my_total_men_count = my_total_men.count()
+
+    # counting Mans clothes
+    my_total_woman = Product_Clothes_Tops.objects.filter(item_gender__iexact="Female")
+    my_total_woman_count = my_total_woman.count()
+
+    # counting Mans clothes
+    my_total_kid = Product_Clothes_Tops.objects.filter(item_gender__iexact="Kids")
+    my_total_kid_count = my_total_kid.count()
+
+    # counting Mans clothes
+    my_total_uni = Product_Clothes_Tops.objects.filter(item_gender__iexact="Unisex")
+    my_total_uni_count = my_total_uni.count()
+
+    queryset_list = Product_Clothes_Tops.objects.order_by('?')
+    #Width
+    if 'width' in request.GET:
+        widths = request.GET['width']
+        widthx = int(widths) + 2
+        if widths:
+            queryset_list = queryset_list.filter(item_width__range=(widths, widthx))
+    #Height
+    if 'height' in request.GET:
+        heights = request.GET['height']
+        heightx = int(heights) + 2
+        if heights:
+            queryset_list = queryset_list.filter(item_height__range=(heights, heightx))
+    #Type
+    if 'Type' in request.GET:
+        types = request.GET['Type']
+        if types:
+            queryset_list = queryset_list.filter(item_category__iexact='sweatshirt')
+    #Country
+    if 'Continent' in request.GET:
+        countries = request.GET['Continent']
+        if countries:
+            queryset_list = queryset_list.filter(item_continent__iexact=countries)
+    queryset_list = queryset_list.filter(item_gender__iexact='Male', item_category__iexact='sweatshirt')
+    paginator = Paginator(queryset_list, 16, orphans=4)
+    my_total_searched = queryset_list.count()
+    page = request.GET.get('page')
+    paginationing = paginator.get_page(page)
+    context = {
+        'CATEGORY_CHOICES': CATEGORY_CHOICES,
+        'GENDER_CHOICES': GENDER_CHOICES,
+        'CONTINENT_CHOICES': CONTINENT_CHOICES,
+        'listing': queryset_list,
+        'my_total': my_total,
+        'values': request.GET,
+        'paginationing': paginationing,
+        'my_total_searched': my_total_searched,
+        'my_total_men_count': my_total_men_count,
+        'my_total_woman_count': my_total_woman_count,
+        'my_total_kid_count': my_total_kid_count,
+        'my_total_uni_count': my_total_uni_count,
+    }
+    return render(request, 'search-mens-tops.html', context)
+
+def search_mens_tops_hoodies(request):
+    listing = Product_Clothes_Tops.objects.all()[:8]
+    my_total = Product_Clothes_Tops.objects.count()
+    # counting Mans clothes
+    my_total_men = Product_Clothes_Tops.objects.filter(item_gender__iexact="Male")
+    my_total_men_count = my_total_men.count()
+
+    # counting Mans clothes
+    my_total_woman = Product_Clothes_Tops.objects.filter(item_gender__iexact="Female")
+    my_total_woman_count = my_total_woman.count()
+
+    # counting Mans clothes
+    my_total_kid = Product_Clothes_Tops.objects.filter(item_gender__iexact="Kids")
+    my_total_kid_count = my_total_kid.count()
+
+    # counting Mans clothes
+    my_total_uni = Product_Clothes_Tops.objects.filter(item_gender__iexact="Unisex")
+    my_total_uni_count = my_total_uni.count()
+
+    queryset_list = Product_Clothes_Tops.objects.order_by('?')
+    #Width
+    if 'width' in request.GET:
+        widths = request.GET['width']
+        widthx = int(widths) + 2
+        if widths:
+            queryset_list = queryset_list.filter(item_width__range=(widths, widthx))
+    #Height
+    if 'height' in request.GET:
+        heights = request.GET['height']
+        heightx = int(heights) + 2
+        if heights:
+            queryset_list = queryset_list.filter(item_height__range=(heights, heightx))
+    #Type
+    if 'Type' in request.GET:
+        types = request.GET['Type']
+        if types:
+            queryset_list = queryset_list.filter(item_category__iexact='Hoodies')
+    #Country
+    if 'Continent' in request.GET:
+        countries = request.GET['Continent']
+        if countries:
+            queryset_list = queryset_list.filter(item_continent__iexact=countries)
+    queryset_list = queryset_list.filter(item_gender__iexact='Male', item_category__iexact='Hoodies')
+    paginator = Paginator(queryset_list, 16, orphans=4)
+    my_total_searched = queryset_list.count()
+    page = request.GET.get('page')
+    paginationing = paginator.get_page(page)
+    context = {
+        'CATEGORY_CHOICES': CATEGORY_CHOICES,
+        'GENDER_CHOICES': GENDER_CHOICES,
+        'CONTINENT_CHOICES': CONTINENT_CHOICES,
+        'listing': queryset_list,
+        'my_total': my_total,
+        'values': request.GET,
+        'paginationing': paginationing,
+        'my_total_searched': my_total_searched,
+        'my_total_men_count': my_total_men_count,
+        'my_total_woman_count': my_total_woman_count,
+        'my_total_kid_count': my_total_kid_count,
+        'my_total_uni_count': my_total_uni_count,
+    }
+    return render(request, 'search-mens-tops.html', context)
+
+def search_mens_tops_jumpers(request):
+    listing = Product_Clothes_Tops.objects.all()[:8]
+    my_total = Product_Clothes_Tops.objects.count()
+    # counting Mans clothes
+    my_total_men = Product_Clothes_Tops.objects.filter(item_gender__iexact="Male")
+    my_total_men_count = my_total_men.count()
+
+    # counting Mans clothes
+    my_total_woman = Product_Clothes_Tops.objects.filter(item_gender__iexact="Female")
+    my_total_woman_count = my_total_woman.count()
+
+    # counting Mans clothes
+    my_total_kid = Product_Clothes_Tops.objects.filter(item_gender__iexact="Kids")
+    my_total_kid_count = my_total_kid.count()
+
+    # counting Mans clothes
+    my_total_uni = Product_Clothes_Tops.objects.filter(item_gender__iexact="Unisex")
+    my_total_uni_count = my_total_uni.count()
+
+    queryset_list = Product_Clothes_Tops.objects.order_by('?')
+    #Width
+    if 'width' in request.GET:
+        widths = request.GET['width']
+        widthx = int(widths) + 2
+        if widths:
+            queryset_list = queryset_list.filter(item_width__range=(widths, widthx))
+    #Height
+    if 'height' in request.GET:
+        heights = request.GET['height']
+        heightx = int(heights) + 2
+        if heights:
+            queryset_list = queryset_list.filter(item_height__range=(heights, heightx))
+    #Type
+    if 'Type' in request.GET:
+        types = request.GET['Type']
+        if types:
+            queryset_list = queryset_list.filter(item_category__iexact='Jumper')
+    #Country
+    if 'Continent' in request.GET:
+        countries = request.GET['Continent']
+        if countries:
+            queryset_list = queryset_list.filter(item_continent__iexact=countries)
+    queryset_list = queryset_list.filter(item_gender__iexact='Male', item_category__iexact='Jumper')
+    paginator = Paginator(queryset_list, 16, orphans=4)
+    my_total_searched = queryset_list.count()
+    page = request.GET.get('page')
+    paginationing = paginator.get_page(page)
+    context = {
+        'CATEGORY_CHOICES': CATEGORY_CHOICES,
+        'GENDER_CHOICES': GENDER_CHOICES,
+        'CONTINENT_CHOICES': CONTINENT_CHOICES,
+        'listing': queryset_list,
+        'my_total': my_total,
+        'values': request.GET,
+        'paginationing': paginationing,
+        'my_total_searched': my_total_searched,
+        'my_total_men_count': my_total_men_count,
+        'my_total_woman_count': my_total_woman_count,
+        'my_total_kid_count': my_total_kid_count,
+        'my_total_uni_count': my_total_uni_count,
+    }
+    return render(request, 'search-mens-tops.html', context)
+
 #
 #
 #
@@ -204,6 +392,199 @@ def search_womens_tops(request):
         if countries:
             queryset_list = queryset_list.filter(item_continent__iexact=countries)
     queryset_list = queryset_list.filter(item_gender__iexact='Female')
+    paginator = Paginator(queryset_list, 16, orphans=4)
+    my_total_searched = queryset_list.count()
+    my_total_searched_sweatshirts = queryset_list.filter(item_category__iexact='sweatshirt', item_gender__iexact='Female').count()
+    my_total_searched_hoodies = queryset_list.filter(item_category__iexact='Hoodies', item_gender__iexact='Female').count()
+    page = request.GET.get('page')
+    paginationing = paginator.get_page(page)
+    context = {
+        'CATEGORY_CHOICES': CATEGORY_CHOICES,
+        'GENDER_CHOICES': GENDER_CHOICES,
+        'CONTINENT_CHOICES': CONTINENT_CHOICES,
+        'listing': queryset_list,
+        'my_total': my_total,
+        'values': request.GET,
+        'paginationing': paginationing,
+        'my_total_searched': my_total_searched,
+        'my_total_searched_sweatshirts': my_total_searched_sweatshirts,
+        'my_total_searched_hoodies': my_total_searched_hoodies,
+        'my_total_men_count': my_total_men_count,
+        'my_total_woman_count': my_total_woman_count,
+        'my_total_kid_count': my_total_kid_count,
+        'my_total_uni_count': my_total_uni_count,
+    }
+    return render(request, 'search-womens-tops.html', context)
+
+def search_womens_tops_sweatshirts(request):
+    listing = Product_Clothes_Tops.objects.all()[:8]
+    my_total = Product_Clothes_Tops.objects.count()
+    # counting Mans clothes
+    my_total_men = Product_Clothes_Tops.objects.filter(item_gender__iexact="Male")
+    my_total_men_count = my_total_men.count()
+
+    # counting Mans clothes
+    my_total_woman = Product_Clothes_Tops.objects.filter(item_gender__iexact="Female")
+    my_total_woman_count = my_total_woman.count()
+
+    # counting Mans clothes
+    my_total_kid = Product_Clothes_Tops.objects.filter(item_gender__iexact="Kids")
+    my_total_kid_count = my_total_kid.count()
+
+    # counting Mans clothes
+    my_total_uni = Product_Clothes_Tops.objects.filter(item_gender__iexact="Unisex")
+    my_total_uni_count = my_total_uni.count()
+
+    queryset_list = Product_Clothes_Tops.objects.order_by('?')
+    #Width
+    if 'width' in request.GET:
+        widths = request.GET['width']
+        widthx = int(widths) + 2
+        if widths:
+            queryset_list = queryset_list.filter(item_width__range=(widths, widthx))
+    #Height
+    if 'height' in request.GET:
+        heights = request.GET['height']
+        heightx = int(heights) + 2
+        if heights:
+            queryset_list = queryset_list.filter(item_height__range=(heights, heightx))
+    #Type
+    if 'Type' in request.GET:
+        types = request.GET['Type']
+        if types:
+            queryset_list = queryset_list.filter(item_category__iexact='sweatshirt')
+    #Country
+    if 'Continent' in request.GET:
+        countries = request.GET['Continent']
+        if countries:
+            queryset_list = queryset_list.filter(item_continent__iexact=countries)
+    queryset_list = queryset_list.filter(item_gender__iexact='Female', item_category__iexact='sweatshirt')
+    paginator = Paginator(queryset_list, 16, orphans=4)
+    my_total_searched = queryset_list.count()
+    page = request.GET.get('page')
+    paginationing = paginator.get_page(page)
+    context = {
+        'CATEGORY_CHOICES': CATEGORY_CHOICES,
+        'GENDER_CHOICES': GENDER_CHOICES,
+        'CONTINENT_CHOICES': CONTINENT_CHOICES,
+        'listing': queryset_list,
+        'my_total': my_total,
+        'values': request.GET,
+        'paginationing': paginationing,
+        'my_total_searched': my_total_searched,
+        'my_total_men_count': my_total_men_count,
+        'my_total_woman_count': my_total_woman_count,
+        'my_total_kid_count': my_total_kid_count,
+        'my_total_uni_count': my_total_uni_count,
+    }
+    return render(request, 'search-womens-tops.html', context)
+
+def search_womens_tops_hoodies(request):
+    listing = Product_Clothes_Tops.objects.all()[:8]
+    my_total = Product_Clothes_Tops.objects.count()
+    # counting Mans clothes
+    my_total_men = Product_Clothes_Tops.objects.filter(item_gender__iexact="Male")
+    my_total_men_count = my_total_men.count()
+
+    # counting Mans clothes
+    my_total_woman = Product_Clothes_Tops.objects.filter(item_gender__iexact="Female")
+    my_total_woman_count = my_total_woman.count()
+
+    # counting Mans clothes
+    my_total_kid = Product_Clothes_Tops.objects.filter(item_gender__iexact="Kids")
+    my_total_kid_count = my_total_kid.count()
+
+    # counting Mans clothes
+    my_total_uni = Product_Clothes_Tops.objects.filter(item_gender__iexact="Unisex")
+    my_total_uni_count = my_total_uni.count()
+
+    queryset_list = Product_Clothes_Tops.objects.order_by('?')
+    #Width
+    if 'width' in request.GET:
+        widths = request.GET['width']
+        widthx = int(widths) + 2
+        if widths:
+            queryset_list = queryset_list.filter(item_width__range=(widths, widthx))
+    #Height
+    if 'height' in request.GET:
+        heights = request.GET['height']
+        heightx = int(heights) + 2
+        if heights:
+            queryset_list = queryset_list.filter(item_height__range=(heights, heightx))
+    #Type
+    if 'Type' in request.GET:
+        types = request.GET['Type']
+        if types:
+            queryset_list = queryset_list.filter(item_category__iexact='Hoodies')
+    #Country
+    if 'Continent' in request.GET:
+        countries = request.GET['Continent']
+        if countries:
+            queryset_list = queryset_list.filter(item_continent__iexact=countries)
+    queryset_list = queryset_list.filter(item_gender__iexact='Female', item_category__iexact='Hoodies')
+    paginator = Paginator(queryset_list, 16, orphans=4)
+    my_total_searched = queryset_list.count()
+    page = request.GET.get('page')
+    paginationing = paginator.get_page(page)
+    context = {
+        'CATEGORY_CHOICES': CATEGORY_CHOICES,
+        'GENDER_CHOICES': GENDER_CHOICES,
+        'CONTINENT_CHOICES': CONTINENT_CHOICES,
+        'listing': queryset_list,
+        'my_total': my_total,
+        'values': request.GET,
+        'paginationing': paginationing,
+        'my_total_searched': my_total_searched,
+        'my_total_men_count': my_total_men_count,
+        'my_total_woman_count': my_total_woman_count,
+        'my_total_kid_count': my_total_kid_count,
+        'my_total_uni_count': my_total_uni_count,
+    }
+    return render(request, 'search-womens-tops.html', context)
+
+def search_womens_tops_jumpers(request):
+    listing = Product_Clothes_Tops.objects.all()[:8]
+    my_total = Product_Clothes_Tops.objects.count()
+    # counting Mans clothes
+    my_total_men = Product_Clothes_Tops.objects.filter(item_gender__iexact="Male")
+    my_total_men_count = my_total_men.count()
+
+    # counting Mans clothes
+    my_total_woman = Product_Clothes_Tops.objects.filter(item_gender__iexact="Female")
+    my_total_woman_count = my_total_woman.count()
+
+    # counting Mans clothes
+    my_total_kid = Product_Clothes_Tops.objects.filter(item_gender__iexact="Kids")
+    my_total_kid_count = my_total_kid.count()
+
+    # counting Mans clothes
+    my_total_uni = Product_Clothes_Tops.objects.filter(item_gender__iexact="Unisex")
+    my_total_uni_count = my_total_uni.count()
+
+    queryset_list = Product_Clothes_Tops.objects.order_by('?')
+    #Width
+    if 'width' in request.GET:
+        widths = request.GET['width']
+        widthx = int(widths) + 2
+        if widths:
+            queryset_list = queryset_list.filter(item_width__range=(widths, widthx))
+    #Height
+    if 'height' in request.GET:
+        heights = request.GET['height']
+        heightx = int(heights) + 2
+        if heights:
+            queryset_list = queryset_list.filter(item_height__range=(heights, heightx))
+    #Type
+    if 'Type' in request.GET:
+        types = request.GET['Type']
+        if types:
+            queryset_list = queryset_list.filter(item_category__iexact='Jumper')
+    #Country
+    if 'Continent' in request.GET:
+        countries = request.GET['Continent']
+        if countries:
+            queryset_list = queryset_list.filter(item_continent__iexact=countries)
+    queryset_list = queryset_list.filter(item_gender__iexact='Female', item_category__iexact='Jumper')
     paginator = Paginator(queryset_list, 16, orphans=4)
     my_total_searched = queryset_list.count()
     page = request.GET.get('page')
