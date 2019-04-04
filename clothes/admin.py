db380b0973b8ca920af9f2cfabbb66554dead29d
Caebix <ma.davidlangr@gmail.com>
2019-04-04 16:34:17 +0200

    upgrade

--- clothes/admin.py
+++ clothes/admin.py
@@ -8,6 +8,6 @@ class PostAdmin(admin.ModelAdmin):
     save_as = True
     list_display = ('item_title', 'item_size', 'item_category','item_color', 'item_asin', 'item_url', 'upload_date', 'item_continent')
     list_filter = ('item_category', 'item_gender', 'item_seller')
-    search_fields = ('item_title', 'item_asin')
+    search_fields = ('item_title', 'item_asin', 'item_brand')
 
 admin.site.register(Product_Clothes_Tops, PostAdmin)
