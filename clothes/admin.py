999adb9fca6c3a40639c9ea43cf76ff9416bd237
Caebix <ma.davidlangr@gmail.com>
2019-04-07 13:28:22 +0200

    lol

--- clothes/admin.py
+++ clothes/admin.py
@@ -6,7 +6,7 @@ from .models import Product_Clothes_Tops
 
 class PostAdmin(admin.ModelAdmin):
     save_as = True
-    list_display = ('item_title', 'item_size', 'item_category','item_color', 'item_url', 'item_continent', 'item_width', 'item_height')
+    list_display = ('item_title', 'item_size', 'item_category','item_color', 'item_url', 'item_continent', 'item_width', 'item_height'. 'upload_date')
     list_filter = ('item_category', 'item_gender', 'item_seller', 'item_continent')
     search_fields = ('item_title', 'item_asin', 'item_brand')
 
